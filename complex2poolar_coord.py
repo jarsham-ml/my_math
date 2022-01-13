7035e37581820dbaf5a34efac7e5ef6d54d6844f
Jarsham Dhir <96953817+jarsham-ml@users.noreply.github.com>
2022-01-13 16:41:21 +0530

    Update complex2poolar_coord.py

--- complex2poolar_coord.py
+++ complex2poolar_coord.py
@@ -1,8 +1,6 @@
 import cmath
 print("Enter complex number. e.g 1+2j") #-1-5j
-real_prt, img_prt = input().split("+")
-img_prt = int(img_prt.replace('j',''))
-complx = complex(int(real_prt),img_prt)
+complx = complex(input())
 # polar coordinates
 print('absolute value(modulus) of complex number')
 print(abs(complx))
